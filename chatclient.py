9156a2a5e62ec854d75d4efd0f3bc2128f0a9d2e
Jamie Katsamatsas <jamie.kats@hotmail.com>
2023-03-25 16:22:14 +1000

    Working on architecture for how many threads and what they will do

--- chatclient.py
+++ chatclient.py
@@ -3,32 +3,66 @@ import sys
 import json
 
 SERVER_NAME = '127.0.0.1'
-SERVER_PORT = 12000
+SERVER_PORT = 14000
+
+MSG_BUFFER_SIZE = 4096
 
 class Client:
     def __init__(self) -> None:
         # load channel info
         self.server_port = int(sys.argv[1])
         self.client_username = sys.argv[2]
-        client_settings = {
+        self.client_settings = {
             "port": self.server_port,
             "username": self.client_username
         }
+
         
-        self.connect_to_server(client_settings)
-        
-        
-    def connect_to_server(self, client_settings: dict):
+    def connect_to_server(self):
         self.client_socket = socket(AF_INET, SOCK_STREAM)
-        self.client_socket.connect((SERVER_NAME, SERVER_PORT))
+        try:
+            self.client_socket.connect((SERVER_NAME, SERVER_PORT))
+        except ConnectionRefusedError as e:
+            print(f"REMOVE WHEN DONE: server socket connection refused: {e}...")
+            exit(1)
+        
+        
+    def start(self):
+        """
+        main thread does nothing (waits for children to finish)
+        child_1
+            - handles user input and passes to outgoing queue
+        child_2
+            - checks outgoing queue and sends outgoing messages
+            - processes incoming messages:
+                - print to screen
+                - shut down client (/kick, /empty)
+                - switch mute flag (/mute))
+        """
+        self.connect_to_server()
         
         # send client username
-        self.client_socket.send(json.dumps(client_settings).encode())
+        self.client_socket.send(json.dumps(self.client_settings).encode())
+        
+        # receive welcome message
+        welcome_msg = self.client_socket.recv(MSG_BUFFER_SIZE)
+        welcome_msg = json.loads(welcome_msg.decode())
+        print(welcome_msg["msg"])
+        
+        # spin up thread to handle incoming/outgoing message queues
+        
+        # listen for server message
+        while True:
+            self.client_socket.recv(MSG_BUFFER_SIZE)
+            
+            print()
+        
+        
         
         
 def main():
     client = Client()
-
+    client.start()
     
         
 if __name__ == "__main__":
--- chatserver.py
+++ chatserver.py
@@ -13,35 +13,79 @@ QUESTIONS
     - if the channel is full does the queue wait time message get printed every
     time the number of clients ahead reduces???
     - are we allowed to use json module
+    - if client is half way through typing a message and a message is received 
+    from their channel do you just print to stdout which would insert 
+    somewhere in the clients typed message
+    - if a client moves from one queue to another does their waiting time reset?
 """
 from socket import *
 from threading import *
 import time
 import sys
 import json
+import queue
+from datetime import datetime
+import signal
+import os
 
 SERVER_NAME = 'server'
-SERVER_PORT = 12000
+SERVER_PORT = 14000
 MSG_BUFFER_SIZE = 4096
 
 CONFIG_COMMAND = 1
 
+# class Message()
+
 class User:
-    def __init__(self, name, connectiion_socket, addr) -> None:
+    def __init__(self, name, port, connectiion_socket, addr) -> None:
         self.name = name
+        self.port = port
         self.connection_socket = connectiion_socket
         self.addr = addr
+        
+    def send_message(self, message_type, message):
+        msg = json.dumps({
+            "msg_type": message_type,
+            "msg": message
+        })
+        self.connection_socket.send(msg.encode())
+    
+    def receive_message(self):
+        pass
 
+class ClientQueue(queue.Queue):
+    def __init__(self, maxsize: int = 0) -> None:
+        super().__init__(maxsize)
+        self.lock = Lock()
+    
+    def remove_user(self, username):
+        """_summary_
+        take lock and dequque to temporary queue, dont add removed user from queue
+        and send users behing them new message of how mnay people there is infront
+        of them
+
+        Args:
+            username (_type_): _description_
+        """
+        pass
+
+    def queue_user(self, ):
+        pass
+    
 class Channel:
     def __init__(self, name, port, max_users) -> None:
         self.name = name
         self.port = port
         self.max_users = max_users
-        self.users = []
-        self.queue = []
+        self.active_users = []
+        self.waiting_queue: ClientQueue = ClientQueue()
         
-    def queue_user(self, ):
-        pass
+    def add_user(self, user: User):
+        self.waiting_queue.put(user)
+        
+    
+    
+    
         
 
 class Server:
@@ -49,6 +93,7 @@ class Server:
         """
         One thread listens for incomming connections
         One thread created for every client connection
+        One thread to handle stdin input
         """
         config_path = sys.argv[CONFIG_COMMAND]
         
@@ -58,14 +103,28 @@ class Server:
         # initialise channels
         self.channels = self.create_channels()
         
+        self.client_threads = []
+        
         # set up listening socket
         self.server_socket = socket(AF_INET, SOCK_STREAM)
         self.server_socket.bind(("localhost", SERVER_PORT))
         self.server_socket.listen()
     
+        # set signal handler
+        signal.signal(signal.SIGINT, self.shut_down)
+        
+    def shut_down(self):
+        # close all network connections
+        # for channel in self.channels:
+        #     for user in self.
+        # # close all threads
         
     def load_config(self, config_path):
-        file = open(config_path, "r")
+        try:
+            file = open(config_path, "r")
+        except OSError as e:
+            print("REMOVE WHEN DONE: config file failed to open...")
+            exit(1)
         lines = file.read().splitlines()
         for line in lines:
             config_options = line.split(" ")
@@ -73,6 +132,13 @@ class Server:
                 "name": config_options[1],
                 "max_users": config_options[3]
                 }
+            
+    def get_time(self):
+        """
+        Returns the time in 24 hour hh:mm:ss format
+        """
+        return datetime.now().strftime("%H:%M:%S")
+            
             
     def create_channels(self):
         """
@@ -92,6 +158,13 @@ class Server:
                 max_users=channel_info["max_users"],
             )
         return channels
+    
+    # def create_channel_queues(self, channels):
+    #     queues = {}
+        
+    #     for channel in channels:
+    #         queues
+        
             
             
     def start(self):
@@ -108,25 +181,39 @@ class Server:
             connection_socket, addr = self.server_socket.accept()
             client_settings = connection_socket.recv(MSG_BUFFER_SIZE).decode()
             client_settings = json.loads(client_settings)
-            # client = User()
+            client = User(
+                name=client_settings["username"],
+                port=client_settings["port"],
+                connectiion_socket=connection_socket,
+                addr=addr)
             # self.channels[client_settings["port"]].add
-                
-            
             
             
             # print(f"accept connection {connection_socket}, {addr}")
             
-            client_thread = Thread(target=self.client_thread, args=(1,))
-            
+            # give client thread reference to channels object
+            client_thread = Thread(target=self.client_thread, args=(self.channels, client,))
+            self.client_threads.append(client_thread)
             client_thread.start()
             
             
-    def client_thread(self, args):
+    def client_thread(self, channels: dict[Channel], client: User):
+        # add client to channel queue
+        print(channels)
+        channel: Channel = channels[str(client.port)]
+        channel.add_user(client)
+        
+        # send client welcome message
+        welcome_msg = f"[Server message ({self.get_time()})] Welcome to the {channel.name} channel, {client.name}."
+        client.send_message("basic", welcome_msg)
+        print(f"Client {client.name} started thread")
+        
         while True:
+            
             print(f"in client thread {self.channels}")
-            time.sleep(0.5)
+            time.sleep(1)
             
-            
+    
         
         
 def main():
